74e649d5aa0832cb1c0e9b0c6888e001c6179a35
yuinore <yuinore@hotmail.co.jp>
2021-02-27 04:58:08 +0900

    implement shuffle

--- tools/generate_image.rb
+++ tools/generate_image.rb
@@ -15,12 +15,15 @@ images = []
 
   images << image
   image.write("#{file_id}_small.png")
+  image.crop(0, 0, image.columns, image.rows).write("#{file_id}_small_crop.png")
 end
 
 ######## Basic Variables
 transparent_color = Pixel.new(0, 0, 0, QuantumRange)
 w = images[0].columns
 h = images[0].rows
+cols = w / sz
+rows = h / sz
 tile = Image.read("img/tile.png").first
 tile = tile.blend(tile, TILE_OPACITY, 0) # change tile opacity
 transparent_tile = tile.blend(tile, 0, 0)
@@ -39,7 +42,7 @@ blks = []
           color = diff.pixel_color(x * sz + i, y * sz + j)
 
           if ([color.red, color.green, color.blue] != [0, 0, 0])
-            blk[y * w + x] = true
+            blk[y * cols + x] = true
             break
           end
         end
@@ -57,7 +60,7 @@ end
   masked = images[image_i2]
   (0...w / sz).each do |x|
     (0...h / sz).each do |y|
-      if blks[image_i1 / 2][y * w + x]
+      if blks[image_i1 / 2][y * cols + x]
         withblock = withblock.composite(tile, x * sz, y * sz, AtopCompositeOp)
       else
         masked = masked.composite(transparent_tile, x * sz, y * sz, CopyCompositeOp)
@@ -66,4 +69,71 @@ end
   end
   withblock.write("#{image_id1}_withblock.png")
   masked.write("#{image_id2}_masked.png")
+end
+
+######## shuffle tiles
+key1 = 9            # gcd(w, key1) == 1
+key2 = 13           # gcd(h, key2) == 1
+key3 = [59, 23, 31] # gcd(w * h, key3[i]) == 1 for all i
+
+def bitshuffle(n, modulo)
+  # FIXME: n must be <2^14
+  subtotal = 0
+  14.downto(0) do |bit|
+    if ((modulo & (1 << bit)) != 0)
+      if (n < subtotal + (1 << bit))
+        n2 = subtotal
+        (0...bit).each do |bit2|
+          n2 += (((n >> (bit - bit2 - 1)) & 1) << bit2)
+        end
+
+        return n2
+      end
+      subtotal += (1 << bit)
+    end
+  end
+
+  raise "Bitshuffle Error"
+end
+
+originals = [
+  "01_withblock.png",
+  "02_masked.png",
+  "03_withblock.png",
+  "04_masked.png",
+]
+
+originals.each do |original|
+  orig = Image.read(original).first
+  shuf = Image.new(w, h) do
+    self.background_color = Pixel.new(0, QuantumRange, 0, 0) #transparent_color
+  end
+
+  (0...w / sz).each do |x|
+    (0...h / sz).each do |y|
+      z = y * cols + x
+      z = (z * key3[0]) % (cols * rows)
+      z = bitshuffle(z, cols * rows)
+      z = (z * key3[1]) % (cols * rows)
+      z = bitshuffle(z, cols * rows)
+      z = (z * key3[2]) % (cols * rows)
+      z = bitshuffle(z, cols * rows)
+      x2 = z % cols
+      y2 = z / cols
+      x2 = (x2 * key1) % cols
+      y2 = (y2 * key2) % rows
+
+      #iportion = orig.crop(x * sz + 1, y * sz, sz, sz) # nazeka 1px zureru
+      #shuf = shuf.composite(portion, x2 * sz, y2 * sz, AtopCompositeOp)
+
+      (0...sz).each do |i|
+        (0...sz).each do |j|
+          color = orig.pixel_color(x * sz + i, y * sz + j)
+          shuf.pixel_color(x2 * sz + i, y2 * sz + j, color)
+        end
+      end
+    end
+  end
+
+  shuf.write("#{File.basename(original, ".*")}_shuffled.png")
 end
