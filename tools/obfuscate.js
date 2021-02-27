0c5b41276147e7739cbb536df12fd27765f12e85
yuinore <yuinore@hotmail.co.jp>
2021-02-28 00:00:21 +0900

    change obfuscator preferences

--- tools/obfuscate.js
+++ tools/obfuscate.js
@@ -12,14 +12,34 @@ const text = fs.readFileSync(filename, 'utf8').toString();
 var obfuscationResult = JavaScriptObfuscator.obfuscate(
   text,
   {
-    compact: false,
+    compact: true,
     controlFlowFlattening: true,
-    controlFlowFlatteningThreshold: 1,
+    controlFlowFlatteningThreshold: 0.75,
+    deadCodeInjection: true,
+    deadCodeInjectionThreshold: 0.4,
+    debugProtection: false, // :-)
+    debugProtectionInterval: false, // :-)
+    disableConsoleOutput: true,
+    identifierNamesGenerator: 'hexadecimal',
+    log: false,
     numbersToExpressions: true,
-    simplify: true,
+    renameGlobals: false,
+    rotateStringArray: true,
+    selfDefending: true,
     shuffleStringArray: true,
+    simplify: true,
     splitStrings: true,
-    stringArrayThreshold: 1
+    splitStringsChunkLength: 10,
+    stringArray: true,
+    stringArrayEncoding: ['rc4'],
+    stringArrayIndexShift: true,
+    stringArrayWrappersCount: 2,
+    stringArrayWrappersChainedCalls: true,
+    stringArrayWrappersParametersMaxCount: 4,
+    stringArrayWrappersType: 'function',
+    stringArrayThreshold: 1,
+    transformObjectKeys: true,
+    unicodeEscapeSequence: false
   }
 );
 
